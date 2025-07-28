4d1bca718e126566b359644310a8c14769de9b58
Marcelo Gomes <marcelogomes.0104@microdatasistemas.com.br>
2025-07-28 09:44:35 -0300

    style: ICrudEndpoint

--- src/ApiVideos.Application/Endpoint/Interface/ICrudEndpoint.cs
+++ src/ApiVideos.Application/Endpoint/Interface/ICrudEndpoint.cs
@@ -5,6 +5,18 @@ namespace ApiVideos.Application.Endpoint.Interface;
 
 public interface ICrudEndpoint<TEntity, TRequest> where TEntity : BaseEntity
 {
+    static abstract Task<IResult> GetByIdAsync(
+        long id,
+        IRepository<TEntity> repository,
+        CancellationToken cancellationToken);
+
+    static abstract Task<IResult> GetAsync(
+        int? pageNumber,
+        int? pageSize,
+        IRepository<TEntity> repository,
+        IMapper mapper,
+        CancellationToken cancellationToken);
+
     static abstract Task<IResult> CreateAsync(
         TRequest request,
         IRepository<TEntity> repository,
@@ -12,13 +24,14 @@ public interface ICrudEndpoint<TEntity, TRequest> where TEntity : BaseEntity
         IMapper mapper,
         CancellationToken cancellationToken);
 
-    static abstract Task<IResult> UpdateAsync(TRequest request, IRepository<TEntity> repository, IMapper mapper, CancellationToken cancellationToken);
-    static abstract Task<IResult> DeleteAsync(long id, IRepository<TEntity> repository, CancellationToken cancellationToken);
-    static abstract Task<IResult> GetAsync(
-        int? pageNumber,
-        int? pageSize,
+    static abstract Task<IResult> UpdateAsync(
+        TRequest request,
         IRepository<TEntity> repository,
         IMapper mapper,
         CancellationToken cancellationToken);
-    static abstract Task<IResult> GetByIdAsync(long id, IRepository<TEntity> repository, CancellationToken cancellationToken);
+
+    static abstract Task<IResult> DeleteAsync(
+        long id,
+        IRepository<TEntity> repository,
+        CancellationToken cancellationToken);
 }
